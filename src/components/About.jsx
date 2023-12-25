e6a54f1c1b01b92953912e2c3358d41458351d5e
kieran <llx.kieran@gmail.com>
2023-12-25 16:26:11 +0100

    update: logo import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Title from './Title'
-import logo1 from '../../public/assets/logo_1.png'
-import logo2 from '../../public/assets/logo_2.png'
-import logo3 from '../../public/assets/logo_3.png'
-import logo4 from '../../public/assets/logo_4.png'
-import logo5 from '../../public/assets/logo_5.png'
-import logo6 from '../../public/assets/logo_6.png'
+import logo1 from '/assets/logo_1.png'
+import logo2 from '/assets/logo_2.png'
+import logo3 from '/assets/logo_3.png'
+import logo4 from '/assets/logo_4.png'
+import logo5 from '/assets/logo_5.png'
+import logo6 from '/assets/logo_6.png'
 
 export default function About() {
 
