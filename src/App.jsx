d391578f8d203d8e0b3b1eced10da4d71499f878
kieran <llx.kieran@gmail.com>
2023-12-30 15:08:46 +0100

    add: lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
 import Header from "./views/Header";
 import About from "./views/About";
 import Hero from "./views/Hero";
 import Work from "./views/Work";
 import Contact from "./views/Contact";
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 
 export default function App() {
+
   return (
-    <>
+    <ReactLenis root>
+    {/* <> */}
     <Header />
     <main
     className="overflow-hidden"
@@ -16,6 +20,9 @@ export default function App() {
       <Work />
       <Contact />
     </main>
-    </>
+    </ReactLenis >
+    // </>
   )
 }
+
+// https://github.com/studio-freight/lenis/tree/main/packages/react-lenis
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,41 @@
+import { useRef, useEffect } from "react";
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Title({content, negative, subtitle}) {
+
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+
+    if(lenisRef) {
+
+      let tl = gsap.timeline({paused: true});
+
+      // console.log(lenisRef.current);
+
+      tl.fromTo(lenisRef.current, {x:0}, {x: '-60%', ease: 'none'}, 0);
+
+      const scrollTitle = ScrollTrigger.create({
+        animation: tl,
+        trigger: lenisRef.current,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.5
+      })
+
+    }
+  }, []);
+
   return (
     <>
       <h2
-      className={`text-mob-title md:text-title font-playfair text-brown mt-40 whitespace-nowrap transform
+      className={`text-mob-title md:text-title font-playfair text-brown mt-40 whitespace-nowrap select-none transform
       ${negative ? "-rotate-little" : "rotate-little"}
       `}
+      ref={lenisRef}
       >&nbsp;{content} - {content} - {content}</h2>
       <h3 className='text-brown text-center md:text-lg'>{subtitle}</h3>
     </>
